644e09b0c3ecb6d59ae123d7992bc3a50f64eca5
Nazar <nazartalaievych@gmail.com>
2026-03-12 17:36:42 +0200

    feat: add email modal window

---
+++ src/features/account/ui/EmailChangeForm.tsx
@@ -0,0 +1,3 @@
+export default function EmailChangeForm() {
+  return <div></div>;
+}
--- src/widgets/account-personal-info/AccountPersonalInfo.tsx
+++ src/widgets/account-personal-info/AccountPersonalInfo.tsx
@@ -5,16 +5,18 @@ import { Pencil } from "lucide-react";
 
 import EditableField from "@features/account/ui/EditableField";
 import PasswordChangeForm from "@features/account/ui/PasswordChangeForm";
+import EmailChangeForm from "@features/account/ui/EmailChangeForm";
 
 import { AppTitle } from "@shared/ui/appTitle";
 import { AppButton } from "@shared/ui/appButton";
 import { AppModal } from "@shared/ui/appModal";
 
 export default function AccountPersonalInfo() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isPasswordModalOpen, setIsPasswordModalOpen] = useState(false);
+  const [isEmailModalOpen, setIsEmailModalOpen] = useState(false);
 
   useEffect(() => {
-    if (isModalOpen) {
+    if (isPasswordModalOpen || isEmailModalOpen) {
       document.body.style.overflow = "hidden";
     } else {
       document.body.style.overflow = "";
@@ -23,11 +25,11 @@ export default function AccountPersonalInfo() {
     return () => {
       document.body.style.overflow = "";
     };
-  }, [isModalOpen]);
+  }, [isPasswordModalOpen, isEmailModalOpen]);
 
   return (
     <>
-      <AppTitle as="h1" size="lg" text="Account" className="mb-4" />
+      <AppTitle as="h1" size="lg" text="Account" className="ml-2 mb-4" />
       <div className="mb-26 py-8 px-6 flex flex-col gap-y-8 bg-surface rounded-2xl">
         <EditableField label="First name" value="John" />
         <EditableField label="Last name" value="Doe" />
@@ -42,7 +44,25 @@ export default function AccountPersonalInfo() {
             intent="outline"
             icon={Pencil}
             iconClasses="w-4.5 h-4.5 md:w-4 md:h-4"
-            onClick={() => setIsModalOpen(true)}
+            onClick={() => setIsPasswordModalOpen(true)}
+            className="px-5"
+          >
+            Change
+          </AppButton>
+        </div>
+
+        <div className="flex justify-between items-center gap-x-4">
+          <div className="flex flex-col justify-start gap-y-2">
+            <p className="capitalize opacity-75">Email</p>
+            <p className="w-37.5 md:w-full font-semibold text-xl truncate">
+              john.doe@gmail.com
+            </p>
+          </div>
+          <AppButton
+            intent="outline"
+            icon={Pencil}
+            iconClasses="w-4.5 h-4.5 md:w-4 md:h-4"
+            onClick={() => setIsEmailModalOpen(true)}
             className="px-5"
           >
             Change
@@ -51,12 +71,21 @@ export default function AccountPersonalInfo() {
       </div>
 
       <AppModal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
+        isOpen={isPasswordModalOpen}
+        onClose={() => setIsPasswordModalOpen(false)}
         titleText="Change Password"
         subtitleText="Fill in the fields below to set a new password for your account."
       >
         <PasswordChangeForm />
+      </AppModal>
+
+      <AppModal
+        isOpen={isEmailModalOpen}
+        onClose={() => setIsEmailModalOpen(false)}
+        titleText="Change Email"
+        subtitleText="Fill in the fields below to set a new password for your account."
+      >
+        <EmailChangeForm />
       </AppModal>
     </>
   );
